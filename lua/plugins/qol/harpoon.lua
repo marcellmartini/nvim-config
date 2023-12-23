16c853224f2af22863e559ad13099081fecf2f33
Marcell Martini <marcellmartini@gmail.com>
2023-12-23 16:13:40 -0400

    Add quality of life plugins
    * comment.nvim
    * harpoon
    * indent-blankline
    * vim-Surround
    * which-key

---
+++ lua/plugins/qol/harpoon.lua
@@ -0,0 +1,15 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim"},
+    config = function()
+        local harpoon = require("harpoon")
+
+        Map("n", "<A-a>", function() harpoon:list():append() end)
+        Map("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+        Map("n", "<A-h>", function() harpoon:list():select(1) end)
+        Map("n", "<A-t>", function() harpoon:list():select(2) end)
+        Map("n", "<A-n>", function() harpoon:list():select(3) end)
+    end
+}
