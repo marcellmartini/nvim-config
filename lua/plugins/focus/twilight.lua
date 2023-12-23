b9e08aefbc9d64620947a91a3df01c84c8f40dbc
Marcell Martini <marcellmartini@gmail.com>
2023-12-23 16:15:07 -0400

    Add focus plugins
    * twilight.nvim
    * zen-mode.nvim

---
+++ lua/plugins/focus/twilight.lua
@@ -0,0 +1,6 @@
+return {
+    "folke/twilight.nvim",
+    config = function()
+        Map("n", "<leader>T", "<cmd> Twilight<CR>")
+    end
+}
---
+++ lua/plugins/focus/zen-mode.lua
@@ -0,0 +1,6 @@
+return {
+    "folke/zen-mode.nvim",
+    config = function()
+        Map("n", "<leader>z", "<cmd> ZenMode<CR>")
+    end
+}
