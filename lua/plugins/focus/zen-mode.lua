e0fbb96d89cdfd09d844e4a6fa017e69606840b2
Marcell Martini <marcellmartini@gmail.com>
2024-01-12 19:22:10 -0400

    Disable twilight when enter in zenmode

--- lua/plugins/focus/zen-mode.lua
+++ lua/plugins/focus/zen-mode.lua
@@ -1,6 +1,12 @@
 return {
 	"folke/zen-mode.nvim",
 	config = function()
+		require("zen-mode").setup({
+			plugins = {
+				twilight = { enabled = false },
+			},
+		})
+
 		Map("n", "<leader>z", "<cmd> ZenMode<CR>")
 	end,
 }
