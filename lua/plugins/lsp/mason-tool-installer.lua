8f9d36be7d5d143ae981b2faf941547c976f69b0
Marcell da Silva Martini-MMARTINI <mmartini@motorola.com>
2024-01-12 18:51:39 -0400

    Add mason-tool-installer plugin

---
+++ lua/plugins/lsp/mason-tool-installer.lua
@@ -0,0 +1,29 @@
+return {
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				-- Fix words
+				"misspell",
+
+				-- Shell
+				"shfmt",
+				"shellcheck",
+
+				-- LUA
+				"stylua",
+
+				-- GO
+				"gofumpt",
+				"goimports-reviser",
+				"golines",
+				"gotests",
+				"impl",
+			},
+		})
+	end,
+}
