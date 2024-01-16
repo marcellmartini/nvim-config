c43dedd9d50c30b9562d25c1e36cbcbebeced986
Marcell Martini <marcellmartini@gmail.com>
2024-01-16 09:30:35 -0400

    Remove todo-comments plugins

--- lua/plugins/layout/todo-comments.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {
-		Map("n", "<leader>st", "<CMD>TodoTelescope<CR>", { desc = "[S]earch [T]odo List" }),
-	},
-	config = true,
-}
