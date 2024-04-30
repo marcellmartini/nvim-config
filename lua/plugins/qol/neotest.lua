af2217dab6d783f4c88553f0942f270b725fce05
Marcell Martini <1556478+marcellmartini@users.noreply.github.com>
2024-04-30 10:56:33 -0400

    feat(neotest): Add keybinds.

--- lua/plugins/qol/neotest.lua
+++ lua/plugins/qol/neotest.lua
@@ -24,5 +24,37 @@ return {
                 require("neotest-go"),
             },
         })
+
+        Map("n", "<leader>tt", function()
+            require("neotest").run.run(vim.fn.expand("%"))
+        end, { desc = "Run File" })
+
+        Map("n", "<leader>tT", function()
+            require("neotest").run.run(vim.uv.cwd())
+        end, { desc = "Run All Test Files" })
+
+        Map("n", "<leader>tr", function()
+            require("neotest").run.run()
+        end, { desc = "Run Nearest" })
+
+        Map("n", "<leader>tl", function()
+            require("neotest").run.run_last()
+        end, { desc = "Run Last" })
+
+        Map("n", "<leader>ts", function()
+            require("neotest").summary.toggle()
+        end, { desc = "Toggle Summary" })
+
+        Map("n", "<leader>to", function()
+            require("neotest").output.open({ enter = true, auto_close = true })
+        end, { desc = "Show Output" })
+
+        Map("n", "<leader>tO", function()
+            require("neotest").output_panel.toggle()
+        end, { desc = "Toggle Output Panel" })
+
+        Map("n", "<leader>tS", function()
+            require("neotest").run.stop()
+        end, { desc = "Stop" })
     end,
 }
