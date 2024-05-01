72cc6123b63915080e2266f185a4fdb0c60f912c
Marcell Martini <1556478+marcellmartini@users.noreply.github.com>
2024-05-01 12:38:32 -0400

    feat(treesitter): Ignore tmux conf files

--- lua/plugins/base/treesitter.lua
+++ lua/plugins/base/treesitter.lua
@@ -15,7 +15,7 @@ return {
                 -- Add languages to be installed here that you want installed for treesitter
                 -- ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
                 ensure_installed = { "lua" },
-                ignore_install = {},
+                ignore_install = { "tmux" },
                 sync_install = false,
                 modules = {},
 
